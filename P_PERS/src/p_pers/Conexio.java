4cc6c25585a3d8a95eb7c8a6c71d61b30f378847
Jonathan Rodriguez <jrodrigue12@milaifontanals.org>
2022-11-06 23:17:51 +0100

    l

---
+++ P_PERS/src/p_pers/Conexio.java
@@ -0,0 +1,29 @@
+package p_pers;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class Conexio {
+
+    private Connection con;
+    
+    private PreparedStatement a;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
